a9b2a47da72a5544c8766d73bb1ef4e8d5550ca3
Bogdan Burlacu <bogdan.burlacu@pm.me>
2026-02-12 11:19:50 +0200

    fix cmake install config

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -1,1 +1,9 @@
+include(CMakeFindDependencyMacro)
+
+# INTERFACE dependencies
+find_dependency(Eigen3 CONFIG REQUIRED)
+find_dependency(tl-expected CONFIG REQUIRED)
+
 include("${CMAKE_CURRENT_LIST_DIR}/lbfgsTargets.cmake")
+check_required_components(lbfgs)
+
